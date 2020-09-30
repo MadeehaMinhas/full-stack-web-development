1f51658e8a70fc42d2b917de6251c1333229e2e3
MadeehaMinhas <minhas.madeeha97@gmail.com>
2020-09-30 19:52:47 +0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+  import React from 'react'
+  import ReactDOM from 'react-dom'
+  const fname="Madeeha"; 
+  const lname="Minhas"; 
+  const series="Netflix series";
+   var d=new Date();
+   var newformat = d.getHours()  >= 12 ? 'PM' : 'AM';
+   var hour=d.getHours();
+   var hours=hour >= 12 ? hour-12 : hour;  
+  ReactDOM.render( 
+    <>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <h1> {`Hello, My name is ${lname},${fname}`}</h1>
+    <p>{`Todays date is ${d.getMonth()+1}/${d.getDate()} /${d.getFullYear() }`  }</p>
+  <p>{`Current time is ${hours}:${d.getMinutes()}:${d.getSeconds()} ${newformat}`  }</p>
+    {/*template literals */  } 
+    
+    <p>{`My first name is ${fname} and last name is ${lname}`}</p>
+  <h1>Thapa Technical  {series}  Pick</h1>
+  <p>List of 5 best series</p>
+  <ol>
+  <li>Dark </li>
+  <li>Extra curricular</li>
+  <li>My Holo Love</li>
+   <li>My First-2 Love</li>
+   <li>Mr. Robot</li>
+  </ol>
+  </>,
+  document.getElementById("root"))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+    
+ 
